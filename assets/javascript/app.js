3f36395be99ac72f82116730f6b2496f72d6e93c
msheffield <msheffield@adaptivebiotech.com>
2019-10-09 22:21:55 -0700

    added background

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,12 +14,17 @@ firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
 // Adds a row to the table with the given variables
-function addToTable(name, destination, frequency) {
+function addToTable(name, destination, frequency, firstTime) {
 
     // Calculate next train time and how many minutes away that is
-    let nextTrainInfo = getTrainTime(frequency, firstime);
+    let nextTrainInfo = getTrainTime(frequency, firstTime);
     let minutesAway = nextTrainInfo[0];
     let nextArrival = nextTrainInfo[1];
+
+    let percentage = (((parseInt(frequency) - parseInt(minutesAway)) / parseInt(frequency)) * 100);
+    percentage = Math.round(percentage);
+    percentageString = percentage + "%";
+    console.log("Percentage: " + percentage);
 
     let row = $("<tr>");
 
@@ -38,29 +43,55 @@ function addToTable(name, destination, frequency) {
     let newNextArrival = $("<td>");
     newNextArrival.text(moment(nextArrival).format("hh:mm"));
     row.append(newNextArrival);
-    
+
     let newMinutesAway = $("<td>");
     newMinutesAway.text(minutesAway);
     row.append(newMinutesAway);
 
+    let progress = $("<td>");
+    let progressdiv = $("<div>");
+    progressdiv.addClass("progress");
+    let progressbar = $("<div>");
+
+    progressbar.attr({ "class": "progress-bar", "role": "progressbar", "style": "width: " + percentageString, "aria-valuenow": percentageString, "aria-valuemin": "0", "aria-valuemax": frequency.toString(), })
+    
+    if (percentage > 80) {
+        progressbar.addClass("bg-success");
+    }
+    else if (percentage < 40) {
+        progressbar.addClass("bg-warning");
+    }
+    else if (percentage < 15) {
+        progressbar.addClass("bg-danger");
+    }
+    else if (percentage == 0) {
+        progressbar.text("Arrived");
+    }
+
+    progressdiv.append(progressbar);
+    progress.append(progressdiv);
+    row.append(progress);
+
+
+    /* progress.html('<div class="progress"><div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">3</div></div>');
+    row.append(progress); */
+
     $("#table-body").append(row);
 }
 
-// On click will grab user inputs, add to table, add to database and clear inputs.
+// On click will grab user inputs, add to database and clear inputs.
 $("#new-train-submit").on("click", function (event) {
     event.preventDefault();
 
     let name = $("#new-train-name").val().trim();
     let destination = $("#new-destination").val().trim();;
-    let firstime = $("#new-first-time").val();
+    let firstTime = $("#new-first-time").val();
     let frequency = $("#new-frequency").val();
-
-    addToTable(name, destination, frequency);
 
     database.ref().push({
         name: name,
         destination: destination,
-        firsttime: firstime,
+        firsttime: firstTime,
         frequency: frequency,
     });
 
@@ -73,24 +104,34 @@ $("#new-train-submit").on("click", function (event) {
 
 
 function getTrainTime(frequency, firstTime) {
-    let firstTimeMoment = moment(firstTime, "HH:mm").subtract(1, "years");
-    console.log(firstTimeMoment);
 
-    var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    let firstTimeMoment = moment(firstTime, "HH:mm").subtract(1, "years");
 
     let differenceInTime = moment().diff(moment(firstTimeMoment), "minutes");
-    console.log("DIFFERENCE IN TIME: " + differenceInTime);
 
     let timeRemainder = differenceInTime % frequency;
-    console.log(timeRemainder);
 
     let minutesTillTrain = frequency - timeRemainder;
-    console.log("MINUTES TILL TRAIN: " + minutesTillTrain);
 
     let nextTrain = moment().add(minutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
     return [minutesTillTrain, nextTrain];
 }
+
+// Load previously stored trains
+database.ref().on("value", function (snapshot) {
+    $("#table-body").empty();
+
+    snapshot.forEach(train => {
+        let name = train.child("name").val();
+        let destination = train.child("destination").val();
+        let firstTime = train.child("firsttime").val();
+        let frequency = train.child("frequency").val();
+
+        addToTable(name, destination, frequency, firstTime);
+    });
+}, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+});
 
